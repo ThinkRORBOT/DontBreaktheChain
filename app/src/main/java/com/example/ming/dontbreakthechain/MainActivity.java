6436627751e00eab6444ca6cbc7a8215b5c80ea2
thinkrorbot <thinkrorbot1236@gmail.com>
2017-07-12 21:32:29 +1000

    Started to create form for user to enter new activity, continued work on reading file,

--- app/src/main/java/com/example/ming/dontbreakthechain/MainActivity.java
+++ app/src/main/java/com/example/ming/dontbreakthechain/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.ming.dontbreakthechain;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,8 +11,10 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ExpandableListView;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private String filename = "userData";
     private InputStream input;
     private File file;
+    private ExpandableListView habitListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,12 +30,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        File dir = new File(getFilesDir()+"/dontbreakthechain");
+        readFile();
 
-        if(!dir.exists()) {
-            dir.mkdir();
-            Log.d("directory", "made");
-        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -38,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Intent intent = new Intent(getApplicationContext(), NewHabitActivity.class);
+                startActivity(intent);
             }
         });
     }
@@ -49,6 +52,25 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    private boolean readFile(){
+        File dir = new File(getFilesDir()+"/dontbreakthechain");
+
+        if(!dir.exists()) {
+            dir.mkdir();
+            Log.d("directory", "made");
+        }
+
+        file = new File(dir.getAbsolutePath() + "/habits.txt");
+
+        if(!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+                messageBox("readFile", e.getMessage());
+            }
+        }
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -62,5 +84,16 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void messageBox(String method, String message) {
+        Log.d("Exception: " + method, message);
+
+        AlertDialog.Builder messageBox = new AlertDialog.Builder(this);
+        messageBox.setTitle(method);
+        messageBox.setMessage(message);
+        messageBox.setCancelable(false);
+        messageBox.setNeutralButton("OK", null);
+        messageBox.show();
     }
 }
